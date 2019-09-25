56f53ecb8b06103f93963214b1e53e4fce07a2e8
jqh <841324345@qq.com>
2019-09-25 10:47:23 +0800

    选中效果优化

--- resources/templates/assets/js/views/toc.js
+++ resources/templates/assets/js/views/toc.js
@@ -13,10 +13,10 @@ function init() {
         return;
     }
 
-    var h2s = $container.find("h2");
-    var h3s = $container.find("h3");
-    var h4s = $container.find("h4");
-    var h5s = $container.find("h5");
+    var h2s = $container.find('h2');
+    var h3s = $container.find('h3');
+    var h4s = $container.find('h4');
+    var h5s = $container.find('h5');
 
     var titles = {h2: h2s.length, h3: h3s.length, h4: h4s.length, h5: h5s.length},
         hTags = [];
@@ -33,12 +33,12 @@ function init() {
 
     function build () {
         // <li style="padding:0 0 0.9rem  0.9rem;color:#a0aec0"><span><i class="fa fa-align-right"></i> MENU</span></li>
-        $("body").prepend('<div class="toc">' +
+        $('body').prepend('<div class="toc">' +
             '<div class="toc-content" id="toc-content"> </div>' +
             '</div>');
 
         var $toc = $('.toc'),
-            $tocContent = $("#toc-content");
+            $tocContent = $('#toc-content');
 
         function setup_container() {
             if (!window.matchMedia(`(min-width:${options.min_width})`).matches) {
@@ -63,7 +63,7 @@ function init() {
             $('.slimScrollDiv').css(heightObj);
         }
 
-        $container.find("h1,h2,h3,h4,h5").each(function (i, item) {
+        $container.find('h1,h2,h3,h4,h5').each(function (i, item) {
             var id = '',
                 tag = $(item).get(0).tagName.toLowerCase(),
                 className = '',
@@ -74,8 +74,8 @@ function init() {
                 id = 'h1-0';
                 className = 'item-h0';
 
-                $(item).attr("id", "target" + id);
-                $(item).addClass("target-name");
+                $(item).attr('id', 'target' + id);
+                $(item).addClass('target-name');
 
                 $tocContent.append('<li><a class="nav-item ' + className + ' anchor-link" onclick="return false;" href="#target' + id + '" link="#target' + id + '">' + text + '</a></li>');
             }
@@ -90,8 +90,8 @@ function init() {
                 id = title + '-' + i + '-' + counter[tag];
                 className = 'item-h' + i;
 
-                $(item).attr("id", "target" + id);
-                $(item).addClass("target-name");
+                $(item).attr('id', 'target' + id);
+                $(item).addClass('target-name');
 
                 $tocContent.append('<li><a class="nav-item ' + className + ' anchor-link" onclick="return false;" href="#target' + id + '" link="#target' + id + '">' + text + '</a></li>');
 
@@ -102,26 +102,51 @@ function init() {
 
         $(window).on('resize', setup_container);
 
-        $toc.find(".anchor-link").click(function () {
-            $("html,body").animate({scrollTop: $($(this).attr("link")).offset().top}, 500);
+        $toc.find('.anchor-link').click(function () {
+            $('html,body').animate({scrollTop: $($(this).attr('link')).offset().top}, 500);
         });
 
-        var tocNavs = $toc.find("li .nav-item");
+        var tocNavs = $toc.find('li .nav-item');
         var tocTops = [];
-        $(".target-name").each(function (i, n) {
+        var scrollable = false;
+
+        $('.target-name').each(function (i, n) {
             tocTops.push($(n).offset().top);
         });
 
+        // 标题点击事件
+        tocNavs.click(function () {
+            var $li = $(this).closest('li');
+
+            // 标记选中的标题项
+            $toc.find('li').removeClass('____');
+            $li.addClass('____');
+
+            // 添加默认选中效果
+            setTimeout(function () {
+                if (scrollable) {
+                    return;
+                }
+                scrollable = false;
+
+                $toc.find('li').removeClass('active');
+                $li.addClass('active');
+            }, 180);
+        });
+
+        // 滚动选中
         $(window).scroll(function () {
-            var scrollTop = $(window).scrollTop();
+            let scrollTop = $(window).scrollTop(),
+                timer;
+            scrollable = true;
 
             $.each(tocTops, function (i, n) {
                 var distance = n - scrollTop,
-                    $item = $(tocNavs[i]);
+                    $item = $(tocNavs[i]).closest('li');
 
                 if (distance >= 0) {
-                    $tocContent.find("li").removeClass('active');
-                    $item.parents('li').addClass('active');
+                    $tocContent.find('li').removeClass('active');
+                    $item.addClass('active').removeClass('____');
                     return false;
                 }
             });
@@ -132,7 +157,30 @@ function init() {
             if (scrollTop + $(window).height() == $(document).height()) {
                 $tocContent.animate({scrollTop: $tocContent.height()}, 100);
             }
+
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                if (! isScrollEnd(scrollTop)) {
+                    return;
+                }
+
+                // 滚动结束后自动选中标记过的标题
+                $.each($tocContent.find('li'), function (k, v) {
+                    let li = $(v);
+                    if (li.hasClass('____')) {
+                        $tocContent.find('li').removeClass('active');
+                        li.addClass('active').removeClass('____');
+                    }
+                });
+
+                scrollable = false;
+            }, 100);
+
         });
+
+        function isScrollEnd(top) {
+            return top == $(window).scrollTop();
+        }
 
         $toc.find('li').eq(0).addClass('active');
     }
